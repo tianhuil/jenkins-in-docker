3110c63850d697082d01817da33b65e7a6917c63
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-11-12 15:47:40 -0500

    caching build

--- images/master/bin/image-build.sh
+++ images/master/bin/image-build.sh
@@ -5,7 +5,7 @@ exit_code=0;
 mkdir -p keys;
 
 cp ../../keys/jenkins.* ./keys &&\
-    docker build --no-cache -t jenkins-master .;
+    docker build -t jenkins-master .;
 if [[ "${?}" != 0 ]]; then
     exit_code=1;
 fi;
