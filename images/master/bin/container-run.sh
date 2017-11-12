82b4b91110d001703a7d3f37f17aaa08c9f692a2
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-11-12 17:05:09 -0500

    For OSX

--- images/master/bin/container-run.sh
+++ images/master/bin/container-run.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+if [[ "$OSTYPE" == "darwin"* ]]; then # OSX
+  DOCKER=$(realpath $(which docker))
+  DOCKER_SOCK=$(realpath /var/run/docker.sock)
+else
+  DOCKER=$(which docker)
+  DOCKER_SOCK=/var/run/docker.sock
+fi
+
 docker run \
     -d \
     --name jenkins-master \
     -p 8080:8080 \
     -p 50000:50000 \
-    -v $(which docker):/usr/bin/docker \
-    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v ${DOCKER}:/usr/bin/docker \
+    -v ${DOCKER_SOCK}:/var/run/docker.sock \
     -v /root/workspace \
     jenkins-master &&\
     docker logs -f jenkins-master;
